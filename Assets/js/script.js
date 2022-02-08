3753f55aae7a76aa1392a6c03a9483ea931750f8
Jordan Ross <jorross@umich.edu>
2022-02-08 00:42:49 -0800

    cleaned up code filling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,40 +2,54 @@ var searchDiv = $('#searchDiv');
 var searchListDiv = $('#searchList');
 
 var index;
+if (localStorage.length > 0){
+    index = localStorage.length
+}
+else {
+    index = 0;
+}
 
-searchDiv.on('click', '#searchBtn', function(event) {
-    event.preventDefault();
-    
+
+loadHistory();
+
+searchDiv.on('click', '#searchBtn', function (event) {
     localStorage.setItem(index, $('#cityInput').val())
 
-    location.reload();
+    $('#currentCity').text(localStorage.getItem(index) + " (" + moment().format('MMMM DD YYYY') + ")");
 
-    $('#currentCity').text($('#cityInput').val() + " (" + moment().format('MMMM DD YYYY') + ")");
+    cleansePage();
+    loadHistory();
 
     index++;
 })
 
-if (localStorage.length > 0) {
-    index = localStorage.length;
-    $('#currentCity').text(localStorage.getItem(index-1) + " (" + moment().format('MM/DD/YYYY') + ")");
-
-    for (var i=0; i<localStorage.length; i++) {
-        searchListDiv.append(
-            '<button class="btn btn-secondary col-9 mx-3 my-2">'+localStorage.getItem(i)+'</button>'
-        )
+function loadHistory() {
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            searchListDiv.append(
+                '<button class="btn btn-secondary col-9 mx-3 my-2">' + localStorage.getItem(i) + '</button>'
+            )
+        }
     }
-    searchListDiv.append(
-        '<button class="btn btn-danger col-9 mx-3 my-2">Clear History</button>'
-    )
+    addClearButton();
 }
-else {
-    index = 0;
+function cleansePage() {
+    $('#searchList').children().remove();
+}
+
+function addClearButton() {
     searchListDiv.append(
         '<button class="btn btn-danger col-9 mx-3 my-2">Clear History</button>'
     )
 }
 
-searchListDiv.on('click', '.btn-danger', function(event) {
+searchListDiv.on('click', '.btn-secondary', function (event) {
+    event.preventDefault();
+
+    $('#currentCity').text($(this).text() + " (" + moment().format('MM/DD/YYYY') + ")");
+})
+
+searchListDiv.on('click', '.btn-danger', function (event) {
     localStorage.clear();
     location.reload();
 })
